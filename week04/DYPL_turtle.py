7ff8999d6f7cd0b7a4248bdf5bf9f339a11a1918
legrand <oceane.legrand@etudiant.univ-lille1.fr>
2017-04-15 05:19:27 -0700

    parseur a jour

--- week04/DYPL_turtle.py
+++ week04/DYPL_turtle.py
@@ -7,6 +7,8 @@ class DYPL_turtle:
     def __init__(self, application):
         self.application = application
     def parseExp(self, text):
-        print (text)
-        print ("\n\n")
-        print (re.sub(r"for (?P<var>\D\d*)=(?P<begin>\d+) to (?P<end>\d+) do\n(?P<statement>.+)\nend", r"for \g<var> in range(\g<begin>,\g<end>):\n\t\g<statement>\n",text))
+        text = (re.sub(r"for (?P<var>\D.*)=(?P<begin>\d+) to (?P<end>\d+) do\n(?P<statement>.+)\nend", r"for \g<var> in range(\g<begin>,\g<end>):\n\t\g<statement>\n",text))
+        text = re.sub(r"(?P<inst1>(pen|move|turn)) (?P<inst2>\D+)",r"\g<inst1>_\g<inst2>",text)
+        text = re.sub(r"(?P<inst1>(pen|move|turn|put))",r"self.\g<inst1>",text)
+        text = re.sub(r"(?P<inst1>(pen|move)_\D+)(?P<esp>[\t|\n| ])",r"\g<inst1>()\g<esp>",text)
+        print(text)
